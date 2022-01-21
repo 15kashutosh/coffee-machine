a043304a535aebf66e673821039a76eaa50abc13
Ashutosh <15kashutosh@gmail.com>
2022-01-21 21:53:01 +0530

    added main method

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
 
 def is_resource_sufficient(order_ingredients):
     """Returns True when order can be made, False if ingredients are insufficient"""
@@ -72,22 +39,55 @@ def make_coffee(drink_name, order_ingredients):
     print(f"Here is your {drink_name} ☕")
 
 
-is_on = True
+if __name__ == '__main__':
+    profit = 0
+    resources = {
+        "water": 300,
+        "milk": 200,
+        "coffee": 100,
+    }
 
-while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino):").lower()
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        print(f"Water : {resources['water']}ml")
-        print(f"Milk : {resources['milk']}ml")
-        print(f"Coffee : {resources['coffee']}ml")
-        print(f"Money : ${profit}")
-    else:
-        drink = MENU[choice]
-        if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink['cost']):
-                make_coffee(choice, drink["ingredients"])
-def timepass():
-    print("Hello")
+    MENU = {
+        "espresso": {
+            "ingredients": {
+                "water": 50,
+                "coffee": 18,
+            },
+            "cost": 1.5,
+        },
+        "latte": {
+            "ingredients": {
+                "water": 200,
+                "milk": 150,
+                "coffee": 24,
+            },
+            "cost": 2.5,
+        },
+        "cappuccino": {
+            "ingredients": {
+                "water": 250,
+                "milk": 100,
+                "coffee": 24,
+            },
+            "cost": 3.0,
+        }
+    }
+
+    is_on = True
+
+    while is_on:
+        choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            print(f"Water : {resources['water']}ml")
+            print(f"Milk : {resources['milk']}ml")
+            print(f"Coffee : {resources['coffee']}ml")
+            print(f"Money : ${profit}")
+        else:
+            drink = MENU[choice]
+            if is_resource_sufficient(drink["ingredients"]):
+                payment = process_coins()
+                if is_transaction_successful(payment, drink['cost']):
+                    make_coffee(choice, drink["ingredients"])
+
